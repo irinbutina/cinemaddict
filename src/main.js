5966acaa31d43e5cf86301e8de7d7259f76cd645
irinbutina <1irinbutina@gmail.com>
2023-02-13 21:32:13 +0700

    feat: break the code into modules

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-import ShowMoreButtonView from './view/show-more-button.js';
+import NavigationView from './view/navigation-view.js';
+import SortView from './view/sort-view.js';
+import FilmsContainerView from './view/films-content-view.js';
+import ShowMoreButtonView from './view/show-more-button-view.js';
+import ProfileView from './view/profile-view.js';
 import { render } from './framework/render.js';
 
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
+
+render (new ProfileView(), siteHeaderElement);
+render (new NavigationView(), siteMainElement);
+render (new SortView(), siteMainElement);
+render (new FilmsContainerView(), siteMainElement);
 
 render (new ShowMoreButtonView(), siteMainElement);
 
